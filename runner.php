0cde0a79bf86763376072061af20081d4a06e40e
Benjamin Eberlei <kontakt@beberlei.de>
2019-11-23 14:26:03 +0100

    Add some debugging output to find out where we are.

--- runner.php
+++ runner.php
@@ -3,3 +3,5 @@
 require_once 'vendor/autoload.php';
 
 echo "Hello world!";
+
+var_dump(array_keys($_SERVER));
--- runner.sh
+++ runner.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+echo "PWD: $PWD\n"
 CURRENT=`dirname $0`
 
 cd $CURRENT
 composer install
-php runner.php
+
+cd -
+php /app/runner.php
