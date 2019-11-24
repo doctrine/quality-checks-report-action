33c6c50ef63f707c272c414594f4c7cf58c0876d
Benjamin Eberlei <kontakt@beberlei.de>
2019-11-24 12:12:36 +0100

    Remove debug output

--- runner.sh
+++ runner.sh
@@ -9,9 +9,5 @@ then
     ./vendor/bin/phpcs --report-checkstyle=/tmp/phpcs.xml
 fi
 
-git log --oneline -n5
-git log $GTHUB_HEAD_REF --oneline -n5
-git log $GTHUB_BASE_REF --oneline -n5
-
 php /app/report.php
 exit $?
